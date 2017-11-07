9c596bfb9e7e05b9908eaa25b8323d770daed5a8
yveso <yveso@users.noreply.github.com>
2017-11-07 22:41:34 +0100

    Activated Redux Dev Tools

--- src/store.js
+++ src/store.js
@@ -7,6 +7,9 @@ const initialState = {
   items
 };
 
-let store = createStore(rootReducer, initialState);
+let store = createStore(
+  rootReducer,
+  initialState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 export default store;
